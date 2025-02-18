7dc5cb12e487cb7b99234a8db4cb32a76dd4289b
LunKEA2024 <maaq0001@stud.kea.dk>
2025-02-18 23:02:57 +0100

    comitting

--- browse.js
+++ browse.js
@@ -1,5 +1,9 @@
 const card_container = document.querySelector("#card_container");
 
+// /*get gender*/
+const pickedGender = new URLSearchParams(window.location.search).get("gender");
+console.log("browse loader...", pickedGender);
+
 fetch(`https://dummyjson.com/users`)
   .then((response) => response.json())
   .then((data) => showList(data.users)); // FIX: Use `data.users`
@@ -9,16 +13,17 @@ function showList(users) { // FIX: Rename `ids` to `users`
     .map(
       (user) => `        
       <div class="card_multi">
-          <img src="${user.image || 'default.png'}" alt="${user.firstName}" class="pfp">
+          <img src="default.png" alt="" class="pfp" id="pfp">
           
           <h1 class="name">
               ${user.firstName} ${user.lastName}
           </h1>
+          <h1> ${user.gender} </h1>
           <p class="job_title">
-              ${user.jobTitle || 'Unknown Job'}
+              ${user.company.title || 'Unknown Job'}
           </p>
 
-          <a href="single.css">Read more</a>
+          <a href="single.html">Read more</a>
       </div>`
     )
     .join("");
@@ -26,6 +31,22 @@ function showList(users) { // FIX: Rename `ids` to `users`
   card_container.innerHTML = markup; // FIX: Use `idContainer`
 }
 
+
+// /*info på browse*/
+// let browse = document.querySelector("card_container");
+// let fetchUrl;
+
+// if (pickedGender === null) {
+//   fetchUrl = https://dummyjson.com/users;
+//   console.log("fetchurl er ", fetchUrl);
+// } else {
+//   fetchUrl = https://dummyjson.com/users/filter?key=gender&value=${pickedGender};
+//   console.log("fetchurl er ", fetchUrl);
+// }
+
+// fetch(fetchUrl)
+//   .then((response) => response.json())
+//   .then((data) => showList(data));
 
 
 
