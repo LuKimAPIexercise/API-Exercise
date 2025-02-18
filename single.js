ccf179fe1a7263d94f6c35c5c0547d51bac9e2c5
taomeria <jojo.razz.kr@gmail.com>
2025-02-19 00:03:24 +0100

    fetch single

--- single.js
+++ single.js
@@ -1,2 +1,22 @@
-fetch(`https://dummyjson.com/users/${profileId}`)
-  .then((response) => response.json())
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+const url = `https://dummyjson.com/users/${id}`;
+const imagePath = `https://dummyjson.com/icon/${id}.webp`;
+
+
+function getUser () {
+  fetch(url)
+    .then((res) => res.json())
+    .then(showUsers);
+}
+
+function showUsers(User) {
+  document.querySelector("h1").textContent = User.name;
+  document.querySelector("p").textContent = User.title;
+  document.querySelector("p").textContent = User.department;
+
+  document.querySelector("img").src =`https://dummyjson.com/icon/${id};.webp`;
+  document.querySelector("img").alt = User.image;
+}
+
+getUser();
